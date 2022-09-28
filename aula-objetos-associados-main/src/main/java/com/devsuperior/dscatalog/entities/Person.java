714aca8cd1c3999a7b7f8d47b615f43c01f94737
Walace da Silva <walacespo@gmail.com>
2022-09-28 08:39:19 -0300

    Project created

---
+++ aula-objetos-associados-main/src/main/java/com/devsuperior/dscatalog/entities/Person.java
@@ -0,0 +1,94 @@
+package com.devsuperior.dscatalog.entities;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_person")
+public class Person implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String name;
+	private Double salary;
+	
+	@ManyToOne
+	@JoinColumn(name = "department_id")
+	private Department department;
+	
+	public Person() {
+	}
+
+	public Person(Long id, String name, Double salary, Department department) {
+		this.id = id;
+		this.name = name;
+		this.salary = salary;
+		this.department = department;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Double getSalary() {
+		return salary;
+	}
+
+	public void setSalary(Double salary) {
+		this.salary = salary;
+	}
+
+	public Department getDepartment() {
+		return department;
+	}
+
+	public void setDepartment(Department department) {
+		this.department = department;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Person other = (Person) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}	
+}
---
+++ aula-objetos-associados-main/src/main/java/com/devsuperior/dscatalog/resources/PersonResource.java
@@ -0,0 +1,33 @@
+package com.devsuperior.dscatalog.resources;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.devsuperior.dscatalog.dto.PersonDTO;
+import com.devsuperior.dscatalog.services.PersonService;
+
+@RestController
+@RequestMapping(value = "/people")
+public class PersonResource {
+
+	@Autowired
+	private PersonService service;
+
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<PersonDTO> findById(@PathVariable Long id) {
+		PersonDTO dto = service.findById(id);
+		return ResponseEntity.ok(dto);
+	}
+	
+	@PostMapping
+	public ResponseEntity<PersonDTO> insert(@RequestBody PersonDTO obj) {
+		//obj = service.insert(obj);
+		return ResponseEntity.ok(obj);
+	}	
+}
